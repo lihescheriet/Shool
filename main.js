af6b3b1cea68881070c6ef67c1ab0c07703c9907
Lihes cheriet <66468238+lihescheriet@users.noreply.github.com>
2021-02-04 16:43:57 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+//Déclarer un tablea listeTodos qui contient deux objets todo
+var listeTodos = [
+    {
+        titre : "Premier todo",
+        description :"un premier todo et sa description"
+    },
+    {
+        titre : "Deuxiéme todo",
+        description : "un deuxieme todo et sa description"
+    }
+];
+console.log(listeTodos);
+//Déclarer une fonction qui affiche un todo dans le document html
+function afficherTodo(todo) {
+    document.write('<b>' + todo.titre + '</b> ');
+    document.write(todo.description);
+    document.write('<br>');
+}
+//Déclarer une fonction qui affiche une liste de todos dans le document html 
+
+//Déclarer une fonction qui crée un objet todo et lajoute a la liste de todos
+
+//Récupéré dans deux variable une référence vers les deux éléments de formulaire
+
+//Déclarer une fonction de callback qui ajoute une nouveau todo contenant les informations 
+
+//Afficher la liste des todos au chargement de la page
